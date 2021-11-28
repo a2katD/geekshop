30e043c510b02a136d8da1d028eb39a32f773eae
a2kat <mousedrw@ya.ru>
2021-11-28 12:04:54 +0300

    lesson4

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -13,18 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
-from mainapp.views import index
+from django.urls import path
+from authapp.views import login, register, logout
 
+app_name = 'authapp'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('mainapp.urls', namespace='mainapp')),
-    path('users/', include('authapp.urls', namespace='authapp')),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
