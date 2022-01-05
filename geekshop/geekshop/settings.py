a8c5b1c6052f1916d3b85fd816aab2e75845ada3
a2kat <mousedrw@ya.ru>
2022-01-05 12:40:46 +0300

    requiremets

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -84,10 +84,18 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.',
+        'NAME': 'geekshop',
+        'USER': 'postgres'
     }
 }
 
